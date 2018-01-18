3eca8a0d42a36d5819b500cca3c1ff5195a66d46
Frédéric Gaurat <fgaurat@eolem.com>
2018-01-18 14:20:18 +0100

    add fragment 2

--- TPRuntimeFragment/app/src/main/java/fr/dmr/tpruntimefragment/tpruntimefragment/MainActivity.java
+++ TPRuntimeFragment/app/src/main/java/fr/dmr/tpruntimefragment/tpruntimefragment/MainActivity.java
@@ -1,7 +1,10 @@
 package fr.dmr.tpruntimefragment.tpruntimefragment;
 
+
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,12 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         BlankFragment blankFragment = new BlankFragment();
-
         //getSupportFragmentManager().beginTransaction().add(android.R.id.content,blankFragment).commit();
-        getSupportFragmentManager().beginTransaction().add(R.id.mainContent,blankFragment).commit();
 
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container,blankFragment).commit();
 
 
     }
+
+    public void clickChangeFragment(View v){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        BlankFragment2 blankFragment2 = new BlankFragment2();
+        transaction.replace(R.id.fragment_container,blankFragment2);
+        transaction.commit();
+    }
+
 }
